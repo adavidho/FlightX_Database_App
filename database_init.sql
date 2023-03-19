42099aa6662360e29f2d027bd1989f15792a8f51
David Hoffmann <david.hoffmann@mail.de>
2023-03-19 20:58:22 +0100

    Added Procedure to databse init

--- database_init.sql
+++ database_init.sql
@@ -28,6 +28,35 @@ LEFT JOIN (
 WHERE f.cancelled = FALSE 
 GROUP BY a.icao_code, a.name;
 
+-- Stored procedure
+CREATE PROCEDURE cancel_flight(flight_number VARCHAR)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  -- Update bookings
+  UPDATE airlinex_booking SET canceled = true WHERE flight_id = flight_number;
+  -- Remove crew assignments for the flight
+  DELETE FROM airlinex_assignment WHERE flight_id = flight_number;
+END;$$;
+
+-- Trigger function to trigger and check for cancellation
+CREATE OR REPLACE FUNCTION cancel_flight_trigger_function()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NEW.canceled THEN
+    CALL cancel_flight(NEW.number);
+  END IF;
+  RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Corresponding trigger
+CREATE TRIGGER cancel_flight_trigger
+AFTER UPDATE ON airlinex_flight
+FOR EACH ROW
+EXECUTE FUNCTION cancel_flight_trigger_function();
+
+-- Inserting Datapoints
 INSERT INTO  public.airlinex_aircraft (registration, type_series, passenger_capacity)
 VALUES 
 ('D-ABYA',	'B748',	364),
